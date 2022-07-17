68c38361faa08b4a6fd8262e414e40ce96046e0d
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-07-17 19:37:38 +0300

    commit tree ca45b1fb6d4f94b3a9b5a41dd2a2e893255e3169

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header({onDarkModeClick, text}){
+    return(
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+            {{text} ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    )
+}
+
+export default Header
